9c6998cea73bf430d143d070381f6267cfaf8f10
Анара Алмазова <anuyka.almaz@gmail.com>
2023-04-17 18:52:34 +0300

    Initial commit

---
+++ spring-types-of-wiring-master/src/main/java/peaksoft/model/Deth8.java
@@ -0,0 +1,12 @@
+package peaksoft.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Deth8 {
+
+
+    public String toString() {
+        return "Finally ";
+    }
+}
